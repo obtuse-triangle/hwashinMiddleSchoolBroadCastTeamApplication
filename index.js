0f0ce75a34e5ce74a5f1ffb643e7c2698fe3606b
NY64 <me@ny64.kr>
2022-11-20 18:03:39 +0900

    asdf

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app = express();
 const port = 40000;
 
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  console.log(`Request received at ${new Date()} from ${req.ip}`);
-  res.sendFile(__dirname + "/public/index.html");
+	console.log(`Request received at ${new Date()} from ${req.ip}`);
+	res.sendFile(__dirname + "/index.html");
 });
+
 app.get("/notapplicationperiod", (req, res) => {
-  console.log(`Request received at ${new Date()} from ${req.ip}`);
-  res.sendFile(__dirname + "/public/notapplicationperiod.html");
+	console.log(`Request received at ${new Date()} from ${req.ip}`);
+	res.sendFile(__dirname + "/notapplicationperiod.html");
 });
+
 app.listen(port, () => console.log(`App is listening on port ${port}`));
